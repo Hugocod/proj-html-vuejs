f35c6981faa0030b2effd6d82c494018bd91fbc5
Hugocod <u.medioli@icloud.com>
2022-10-09 09:44:59 +0200

    fixed ctaSection

--- src/components/primary/CtaSection.vue
+++ src/components/primary/CtaSection.vue
@@ -1,13 +1,39 @@
 <template>
-    <div></div>
+    <div class="container">
+        <div class="wrapper">
+            <text-block :classVariant="1" title="Request A Free Quote" />
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus.
+            </p>
+            <btn-mono buttonText="click here" />
+        </div>
+    </div>
 </template>
 
 <script>
+import BtnMono from "../secondary/BtnMono.vue";
+import TextBlock from "../secondary/TextBlock.vue";
 export default {
+    components: { TextBlock, BtnMono },
     name: "CtaSection",
 };
 </script>
 
 <style scoped lang="scss">
 @import "../../scss/_resources.scss";
+
+.container {
+    background-image: url("../../../public/images/avada-movers-request-quote-background.jpg");
+    border-bottom: 15px solid $color-green-vivid;
+    .wrapper {
+        @include wrapper-size;
+        padding: 6rem 0rem;
+        width: 60%;
+
+        p {
+            padding: 2rem;
+        }
+    }
+}
 </style>
