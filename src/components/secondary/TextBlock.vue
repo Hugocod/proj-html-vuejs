36e584bcf8535ecaba7f56529d2d670c8b678e99
Hugocod <u.medioli@icloud.com>
2022-10-09 09:45:17 +0200

    added new variant in textBlock

--- src/components/secondary/TextBlock.vue
+++ src/components/secondary/TextBlock.vue
@@ -10,7 +10,7 @@ export default {
     name: "TextBlock",
     data() {
         return {
-            availableVariants: ["variant-1", "variant-2"], // a ognuna delle classi corrisponde una variante del componente
+            availableVariants: ["variant-1", "variant-2", "variant-3"], // a ognuna delle classi corrisponde una variante del componente
         };
     },
     props: { classVariant: Number, title: String, subtitle: String },
@@ -56,6 +56,25 @@ export default {
 
     h2 {
         @include title-big;
+    }
+}
+/////////////////////////////////////////////////////////////////// VARIANT-----/
+
+/////////////////////////////////////////////////////////////////// VARIANT-3
+.variant-3 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    h4 {
+        @include title-small;
+        color: $color-white;
+    }
+
+    h2 {
+        @include title-medium;
+        color: $color-white;
     }
 }
 /////////////////////////////////////////////////////////////////// VARIANT-----/
