78673d65cc9ec4cafd89b5287a5348e5168b3d5e
Hugocod <u.medioli@icloud.com>
2022-10-09 09:43:40 +0200

    completed InfoMovingSection

--- src/components/primary/InfoSectionMoving.vue
+++ src/components/primary/InfoSectionMoving.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="container">
         <div class="wrapper">
-            <text-block :classVariant="1" title="Request A Free Quote" />
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis
-                parturient montes, nascetur ridiculus mus.
-            </p>
-            <btn-mono buttonText="click here" />
+            <div class="empty-box"></div>
+            <div class="green-box">
+                <text-block class="t-block" :classVariant="3" subtitle="we remove the headache from moving" title="Move With Ease" />
+                <div class="line"></div>
+                <p>
+                    With our super simple service we can take you from getting a quote, right through to being in your new home with all of your
+                    belongings securely packed and safely delivered, even in the rooms of your choice.
+                </p>
+                <btn-mono buttonText="click here" />
+            </div>
         </div>
     </div>
 </template>
@@ -24,15 +28,44 @@ export default {
 @import "../../scss/_resources.scss";
 
 .container {
-    background-image: url("../../../public/images/avada-movers-request-quote-background.jpg");
-    border-bottom: 15px solid $color-green-vivid;
+    background-image: url("https://avada.theme-fusion.com/movers/wp-content/uploads/sites/108/2018/06/avada-movers-movewithease-background-new.jpg");
+    background-size: cover;
+    padding: 7rem 0;
+
     .wrapper {
         @include wrapper-size;
-        padding: 6rem 0rem;
-        width: 60%;
+        padding: 2rem 0rem;
 
-        p {
-            padding: 2rem;
+        display: flex;
+        justify-content: space-between;
+
+        .empty-box {
+            width: 0%;
+        }
+
+        .green-box {
+            width: 55%;
+            background-color: $color-green-vivid;
+            color: $color-white;
+            border: 1rem solid $color-green-darker;
+            padding: 2.5rem 0;
+
+            @media screen and (max-width: 1024px) {
+                width: 100%;
+            }
+
+            .line {
+                height: 0.01rem;
+                margin: 0 auto;
+                margin-top: 2rem;
+
+                width: 70%;
+                background-color: rgb(81, 255, 81);
+            }
+
+            p {
+                padding: 2rem;
+            }
         }
     }
 }
